c835a4a8c3ad09765aeb349683a57e9b8533a9da
Bradenmcclean <mcclean.braden@gmail.com>
2020-09-23 14:35:22 -0600

    moving ship with boundries

--- Main.cpp
+++ Main.cpp
@@ -9,14 +9,16 @@
 #include <SFML/Graphics.hpp>
 #include "GalacticaClass.cpp"
 #include "EnemiesClass.cpp"
-
+#include "SpaceShipPlayer.cpp"
 int main()
 {
     // create the window
-    sf::RenderWindow window(sf::VideoMode(2800, 3000), "My window");
+    float windowX=2800;
+    sf::RenderWindow window(sf::VideoMode(windowX, 3000), "My window");
 
     sf::CircleShape shape(50.f);
-    
+    //create my space ship boi
+    spaceShip player=spaceShip("GalagaSpaceShip.png",1450,1400);
     // Create a vector of enemies
     vector<Enemy> enemies;
     float x = 500;
@@ -52,12 +54,48 @@ int main()
                     if (event.key.code==sf::Keyboard::D)
                     {
                         shape.move(sf::Vector2f(5,0));
+                        //this is for fuild movement
+//                        if (event.key.code==sf::Keyboard::D&& velocity < maxVel) // max vel is something like 6
+//                              velocity += acceleration; // something like 0.1
+//                        sprite.Move( velocity, 0 );
+                        //this if statment sees if the key is pressed and
+                        //keeps the speed to a max velocity
+                        if (event.key.code==sf::Keyboard::D&& player.velocity<player.maxPostiveVelocity)
+                        {
+                            //the velocity and the accleration are in
+                            //the class
+                            player.velocity+=player.postiveAccleration;
+                            player.checkBoundandMove(windowX);
+                        }
+                        else
+                        {
+                            //keeps the thing moveing after reaching maxVelocity
+                            player.checkBoundandMove(windowX);
+                        }
+                
                     }
                     if (event.key.code==sf::Keyboard::A)
                     {
                         shape.move(sf::Vector2f(-5,0));
+                        
+                        //flip all the signs
+                        if (event.key.code==sf::Keyboard::A&& player.velocity>player.maxNegativeVelocity)
+                        {
+                            //the velocity and the accleration are in
+                            //the class
+                            player.velocity+=player.negativeAccleration;
+                             player.checkBoundandMove(windowX);
+                        }
+                        else
+                        {
+                            //keeps the thing moveing after reaching maxVelocity
+                             player.checkBoundandMove(windowX);
+                        }
                     }
                 }
+              
+           
+                
            }
 
            // clear the window with black color
@@ -70,7 +108,14 @@ int main()
            
        // Draw the enemy squad in the window
        myEnemySquad.drawEnemySquad(window);
-
+           
+            player.velocityToZero();
+             player.checkBoundandMove(windowX);
+             //slowly changes ship speeITS SUPER RAD
+             //needs to be in this or the players speed looks like shit
+           
+        //Draws the spaceShip
+           player.drawSpaceShip(window);
        // end the current frame
        window.display();
        }
